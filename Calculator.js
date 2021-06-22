407fa4054a8002d7f3dc6e2a7a73b920cdf2e186
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-22 07:53:06 -0500

    Create a class called Calculator which will be the main part of the project

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+class Calculator {
+  add(numberA, numberB) {
+    return numberA + numberB;
+  }
+  substract(numberA, numberB) {
+    return numberA + numberB;
+  }
+  multiply(numberA, numberB) {
+    return numberA * numberB;
+  }
+  divide(numberA, numberB) {
+    return numberA / numberB;
+  }
+}
