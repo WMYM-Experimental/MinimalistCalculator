2b21124ae25992003df4c1feb6f7dfa72d32a89a
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-07-15 16:28:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const calcScreenPreviousValue = document.getElementById("previous-value");
 const calcScreenActualValue = document.getElementById("actual-value");
 
-const BUTTONS_NUMBERS = document.querySelectorAll("number");
-const BUTTONS_OPERATORS = document.querySelectorAll("operator");
+const BUTTONS_NUMBERS = document.querySelectorAll("number"); //all the buttons with number class
+const BUTTONS_OPERATORS = document.querySelectorAll("operator"); //all the buttons with operator class
 
-const screen = new Screen(calcScreenPreviousValue, calcScreenActualValue);
+const screen = new Screen(calcScreenPreviousValue, calcScreenActualValue); //display the values
 
-BUTTONS_NUMBERS.forEach(boton => {
-    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+BUTTONS_NUMBERS.forEach(b => {
+    boton.addEventListener('click', () => display.addNumber(b.innerHTML));
 });
 
-botonesOperadores.forEach(boton => {
-    boton.addEventListener('click', () => display.computar(boton.value))
+BUTTONS_OPERATORS.forEach(b => {
+    boton.addEventListener('click', () => display.compute(b.value))
 });
